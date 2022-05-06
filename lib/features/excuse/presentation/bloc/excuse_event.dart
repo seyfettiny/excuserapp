84c8e7accf86ee2544f916ba4f3d82c831e467ca
seyfettiny <334321@ogr.ktu.edu.tr>
2022-05-06 11:00:19 +0300

    Create excuse bloc

---
+++ lib/features/excuse/presentation/bloc/excuse_event.dart
@@ -0,0 +1,4 @@
+part of 'excuse_bloc.dart';
+
+@immutable
+abstract class ExcuseEvent {}
---
+++ lib/features/excuse/presentation/bloc/excuse_state.dart
@@ -0,0 +1,6 @@
+part of 'excuse_bloc.dart';
+
+@immutable
+abstract class ExcuseState {}
+
+class ExcuseInitial extends ExcuseState {}
