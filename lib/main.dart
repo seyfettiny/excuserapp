dc63fcbd530594b8490a5494432bdd0314a9e57a
seyfettiny <334321@ogr.ktu.edu.tr>
2022-05-15 17:16:58 +0300

    Add launcher icons for iOS and Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import 'locator.dart';
 import 'features/excuse/presentation/router.dart';
