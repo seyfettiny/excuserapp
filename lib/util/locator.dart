ca2dbdc9fed63dcc4cd885a3aae2005d6510719f
Seyfettin Yıldırım <334321@ogr.ktu.edu.tr>
2022-12-20 19:50:59 +0300

    Move locator to the util layer

---
+++ lib/util/locator.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import '../data/datasources/local/database.dart';
+import '../data/datasources/remote/excuser_api.dart';
+import '../data/repositories/excuse_repository.dart';
+import '../domain/repositories/excuse_repository.dart';
+import '../domain/usecases/get_random_excuse.dart';
+import '../domain/usecases/get_random_excuse_by_category.dart';
+import '../presentation/cubit/randomcategoryexcuse/cubit/random_category_excuse_cubit.dart';
+import '../presentation/cubit/randomexcuse/random_excuse_cubit.dart';
+
+final locator = GetIt.instance;
+void setupLocator() {
+  locator.registerLazySingleton<IExcuseRepository>(
+      () => ExcuseRepository(locator(), locator(), locator()));
+  locator.registerLazySingleton(() => GetRandomExcuseUseCase(locator()));
+  locator.registerLazySingleton(() => GetRandomExcuseByCategoryUseCase(locator()));
+  locator.registerLazySingleton(() => ExcuserAPI(locator()));
+  locator.registerLazySingleton(() => ExcuseDatabase(_openConnection()));
+
+  locator.registerFactory(() => RandomExcuseCubit(locator(),));
+  locator.registerFactory(() => RandomCategoryExcuseCubit(locator(),));
+  locator.registerSingleton<String>(Platform.localeName);
+  locator.registerLazySingleton(() => InternetConnectionChecker());
+  locator.registerSingleton<SupabaseClient>(
+      SupabaseClient(dotenv.env['API_URL']!, dotenv.env['API_KEY']!));
+}
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(path.join(dbFolder.path, 'db.sqlite'));
+    return NativeDatabase.createInBackground(file);
+  });
+}
