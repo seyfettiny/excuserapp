7da96450393cfef341cf564bb2cc9ca3a6ebb37a
Seyfettin Yıldırım <334321@ogr.ktu.edu.tr>
2022-12-23 22:25:23 +0300

    Add tests for RandomCategoryExcuse Cubit

---
+++ test/presentation/cubit/randomcategoryexcuse/cubit/random_category_excuse_cubit_test.dart
@@ -0,0 +1,79 @@
+import 'package:excuserapp/data/models/excuse_model.dart';
+import 'package:excuserapp/domain/usecases/get_random_excuse_by_category.dart';
+import 'package:excuserapp/presentation/cubit/randomcategoryexcuse/cubit/random_category_excuse_cubit.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'random_category_excuse_cubit_test.mocks.dart';
+
+@GenerateMocks([
+  GetRandomExcuseByCategoryUseCase,
+])
+void main() {
+  late RandomCategoryExcuseCubit cubit;
+  late MockGetRandomExcuseByCategoryUseCase mockUseCase;
+
+  setUp(() {
+    mockUseCase = MockGetRandomExcuseByCategoryUseCase();
+    cubit = RandomCategoryExcuseCubit(mockUseCase);
+  });
+
+  group('getRandomExcuseByCategory', () {
+    final tExcuse =
+        ExcuseModel(id: 0, excuse: 'Test Excuse', category: 'family');
+    test('emits RandomCategoryExcuseLoading', () {
+      // arrange
+      when(mockUseCase.execute(any)).thenAnswer((_) async => tExcuse);
+
+      // act
+      cubit.getRandomExcuseByCategory();
+
+      // assert
+      expect(cubit.state, equals(RandomCategoryExcuseLoading()));
+    });
+
+    test('emits RandomCategoryExcuseLoaded', () {
+      // arrange
+      when(mockUseCase.execute(any)).thenAnswer((_) async => tExcuse);
+      // assert
+      expectLater(cubit, emitsInOrder([
+        RandomCategoryExcuseInitial(),
+        RandomCategoryExcuseLoading(),
+        RandomCategoryExcuseLoaded(excuse: tExcuse),
+      ]));
+      // act
+      cubit.getRandomExcuseByCategory();
+
+
+    });
+
+    test('emits RandomCategoryExcuseError', () {
+      // arrange
+      when(mockUseCase.execute(any)).thenThrow(Exception('error'));
+
+      // act
+      cubit.getRandomExcuseByCategory();
+
+      // assert
+      expect(cubit.state,
+          equals(const RandomCategoryExcuseError('Exception: error')));
+    });
+  });
+
+  group('changeCategory', () {
+    final tExcuse =
+        ExcuseModel(id: 0, excuse: 'Test Excuse', category: 'family');
+
+    test('updates category field', () {
+      // arrange
+      when(mockUseCase.execute(any)).thenAnswer((_) async => tExcuse);
+
+      // act
+      cubit.changeCategory('work');
+
+      // assert
+      expect(cubit.category, equals('work'));
+    });
+  });
+}
