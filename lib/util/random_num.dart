05c4104696d14270b7ae88585e2c30c827914f3b
Seyfettin Yıldırım <334321@ogr.ktu.edu.tr>
2022-12-26 22:03:28 +0300

    Fix typo.

--- lib/util/random_num.dart
+++ lib/util/random_num.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 extension RandomNum on int {
   static int random(int min, int max) {
-    final _random = Random();
-    return min + _random.nextInt(max - min);
+    final random = Random();
+    return min + random.nextInt(max - min);
   }
 }
