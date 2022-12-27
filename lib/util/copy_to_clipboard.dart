f4252ed9277847594ae3398c731b435600267a45
Seyfettin Yıldırım <334321@ogr.ktu.edu.tr>
2022-12-27 23:07:28 +0300

    Add mount flag for unwanted context issues.

--- lib/util/copy_to_clipboard.dart
+++ lib/util/copy_to_clipboard.dart
@@ -5,8 +5,10 @@ import '../constants/app_constants.dart';
 import 'get_locale.dart';
 
 class CopyClipboard {
-  static Future<void> copyToClipboard(BuildContext context, String text) async {
+  static Future<void> copyToClipboard(BuildContext context, String text,
+      [bool mounted = true,]) async {
     await Clipboard.setData(ClipboardData(text: text));
+    if (!mounted) return;
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
       content: Text(GetLocale.getLocale() == 'en'
           ? AppConstants.copiedEN
